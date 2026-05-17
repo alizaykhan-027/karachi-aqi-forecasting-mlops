707c2f1f230e097037fc77db1159a33cf4ff2263
alizaykhan-027 <alizayk027@gmail.com>
2026-05-17 09:26:15 -0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,36 @@
+import pandas as pd
 
+# ============================================================
+# CLEAN TIMESTAMP
+# ============================================================
+
+def clean_timestamp(df):
+
+    df["timestamp"] = pd.to_datetime(
+        df["timestamp"]
+    )
+
+    return df.sort_values(
+        "timestamp"
+    ).reset_index(drop=True)
+
+# ============================================================
+# REMOVE INF
+# ============================================================
+
+def remove_invalid(df):
+
+    return df.replace(
+        [float("inf"), float("-inf")],
+        pd.NA
+    )
+
+# ============================================================
+# PRINT SECTION
+# ============================================================
+
+def print_section(title):
+
+    print("\n" + "=" * 60)
+    print(title)
+    print("=" * 60)
